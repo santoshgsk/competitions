18654136707cd9e0243fbcc04dd7800699f5a87b
santoshgsk <gsk.krishna@gmail.com>
2019-07-24 21:15:57 +0530

    cookiecutter project commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repository of ML tools and tricks',
+    author='Santosh GSK',
+    license='MIT',
+)
